3eb7a1d11752a39729451694d3c8b278062c32e8
Benoît Minisini <gambas@users.sourceforge.net>
2009-11-15 13:46:01 +0000

    [DEVELOPMENT ENVIRONMENT] * BUG: The make executable dialog options are correctly refreshed depending   on whether the project is a component or not. * BUG: Removing a source file removes correctly the translation file from   version control too now.
    [EXAMPLES]
    * BUG: The hour hand follows the minutes now in the Clock example.
    
    [INTERPRETER]
    * BUG: The thousand separator is only printed between digits now.
    
    
    git-svn-id: svn://localhost/gambas/branches/2.0@2419 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- gb.qt/share/gb.form.properties.h
+++ gb.qt/share/gb.form.properties.h
@@ -62,7 +62,7 @@
 #define CHSPLIT_PROPERTIES "*"
 #define CVSPLIT_PROPERTIES "*"
 #define CFRAME_PROPERTIES "*,Text"
-#define CTABSTRIP_PROPERTIES "*," CARRANGEMENT_PROPERTIES ",Count{Range:1;256}=1,Index,Text,Picture,Orientation{TabStrip.Top;Bottom}"
+#define CTABSTRIP_PROPERTIES "*," CARRANGEMENT_PROPERTIES ",Count{Range:1;256}=1,Index,Text,Picture,Orientation{TabStrip.Top;Bottom}=Top"
 #define CWINDOW_PROPERTIES "*,Action,Text,Icon,Picture,Mask,Persistent,Border{Window.None;Fixed;Resizable}=Resizable,ToolBox,Stacking{Window.Normal;Above;Below}=Normal,Minimized,Maximized,FullScreen,Sticky,SkipTaskbar," CARRANGEMENT_PROPERTIES
 #define CMOVIEBOX_PROPERTIES "*,Path,Playing,Border{Border.*}"
 #define CPROGRESSBAR_PROPERTIES "*,Label=True"
