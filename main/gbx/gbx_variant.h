5ea9a4ecd4c657c84221e4f718081ab69982dd44
Benoît Minisini <gambas@users.sourceforge.net>
2009-11-28 09:08:25 +0000

    [GB.FORM] * BUG: TableView cannot raise a spurious "Null object" error anymore when   calling the Edit() method.
    git-svn-id: svn://localhost/gambas/branches/2.0@2445 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- main/gbx/gbx_variant.h
+++ main/gbx/gbx_variant.h
@@ -4,7 +4,7 @@
 
   Variant management routines
 
-  (c) 2000-2005 Beno�t Minisini <gambas@users.sourceforge.net>
+  (c) 2000-2009 Benoît Minisini <gambas@users.sourceforge.net>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
