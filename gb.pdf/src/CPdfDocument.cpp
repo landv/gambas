a9120e0ab134b5f186f5a9682434c92d938673f8
Benoît Minisini <gambas@users.sourceforge.net>
2010-02-17 23:12:43 +0000

    [GB.PDF] * BUG: PDF indexes should be read correctly now.
    git-svn-id: svn://localhost/gambas/branches/2.0@2704 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- gb.pdf/src/CPdfDocument.cpp
+++ gb.pdf/src/CPdfDocument.cpp
@@ -169,25 +169,33 @@ static LinkDest *get_dest(LinkAction *act)
 
 static uint32_t aux_get_page_from_action(void *_object, LinkAction *act)
 {
-	// TODO: NamedDest
-
-	Ref pref;	
+	Ref pref;       
 	LinkDest *dest = get_dest(act);
+	GooString *name;
+
+	if (!dest)
+	{
+		// try to use NamedDest to get dest
+		if (!act)
+			return 0;
+		if (act->getKind () == actionGoTo)
+		{
+			name = ((LinkGoTo*)act)->getNamedDest();
+			if (name)
+				dest = THIS->doc->findDest(name);
+		}
+	}
+
 	if (!dest)
 		return 0;
 
-	if (dest->isPageRef())
+	if (dest->isPageRef() )
 	{
-		if (dest->isPageRef() )
-		{
-			pref= dest->getPageRef();
-			return THIS->doc->findPage(pref.num, pref.gen);
-		}
-		else
-			return dest->getPageNum();
+		pref= dest->getPageRef();
+		return THIS->doc->findPage(pref.num, pref.gen);
 	}
 	else
-		return 0;
+		return dest->getPageNum();
 }
 
 static void aux_get_dimensions_from_action(LinkAction *act,int *left, int32_t *right, int32_t *top, int32_t *bottom)
