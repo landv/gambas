a01e8c1d7295bc3d6b3d7f086ba7e00d140fd8e7
Benoît Minisini <gambas@users.sourceforge.net>
2010-06-13 11:04:57 +0000

    [ARCHIVER] * NEW: Ignore valgrind core files and callgrind output files when creating   an archive.
    git-svn-id: svn://localhost/gambas/trunk@3002 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- main/gbc/gba.c
+++ main/gbc/gba.c
@@ -302,6 +302,12 @@ int main(int argc, char **argv)
 					if ((len > 5) && (strncmp(file_name, "core.", 5) == 0))
 						continue;
 
+					if ((len > 7) && (strncmp(file_name, "vgcore.", 5) == 0))
+						continue;
+
+					if ((len > 10) && (strncmp(file_name, "callgrind.", 5) == 0))
+						continue;
+
 					file = FILE_cat(path, file_name, NULL);
 
 					if (stat(file_name, &info))
