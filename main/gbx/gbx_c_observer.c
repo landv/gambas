1bd45a5751396f4be60b6aa021776c65b5a20431
Benoît Minisini <gambas@users.sourceforge.net>
2010-12-21 14:35:39 +0000

    [INTERPRETER] * NEW: Forgot to add two source files.
    git-svn-id: svn://localhost/gambas/trunk@3388 867c0c6c-44f3-4631-809d-bfa615b0a4ec

---
+++ main/gbx/gbx_c_observer.c
@@ -0,0 +1,169 @@
+/***************************************************************************
+
+  gbx_c_observer.c
+
+  (c) 2000-2009 Benoît Minisini <gambas@users.sourceforge.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+***************************************************************************/
+
+#define __GBX_C_OBSERVER_C
+
+#include "gbx_info.h"
+
+#ifndef GBX_INFO
+
+#include "gbx_exec.h"
+#include "gbx_api.h"
+#include "gbx_event.h"
+#include "gbx_c_observer.h"
+
+void COBSERVER_attach(COBSERVER *this, void *parent, const char *name)
+{
+	//fprintf(stderr, "COBSERVER_attach: %p: %s %p\n", this, parent ? OBJECT_class(parent)->name : "", parent);
+	//COBSERVER_set_parent(this, parent);
+	if (this->event)
+		EVENT_search(OBJECT_class(this->object), this->event, name, parent);	
+}
+
+void COBSERVER_detach(COBSERVER *this)
+{
+	//fprintf(stderr, "COBSERVER_detach: %p\n", this);
+	//COBSERVER_set_parent(this, NULL);
+	
+	//this->proxy = NULL;
+	
+	if (this->event)
+		FREE(&this->event, "COBSERVER_detach");
+}
+
+BEGIN_METHOD(Observer_new, GB_OBJECT object; GB_BOOLEAN after)
+
+	OBJECT *object;
+	void *proxy = NULL;
+	OBJECT_EVENT *ev;
+	char *name;
+	CLASS *class;
+	void *parent;
+	
+	object = (OBJECT *)VARG(object);
+	if (GB_CheckObject(object))
+		return;
+	
+	#if 0
+	if (OBJECT_class(THIS) == CLASS_Proxy && OBJECT_has_events(OP))
+		proxy = OP;
+	#endif
+	
+	if (proxy)
+	{
+		//fprintf(stderr, "proxy = (%s %p) %s %s\n", GB_GetClassName(proxy), proxy, EVENT_Name, EVENT_PreviousName);
+		parent = OBJECT_parent(proxy);
+		name = EVENT_PreviousName;
+	}
+	else
+	{
+		parent = OBJECT_parent(THIS);
+		name = EVENT_Name;
+	}
+	
+	if (!parent)
+		return;
+	
+	class = OBJECT_class(object);
+	if (class->n_event == 0)
+		return;
+	
+	if (!name || !*name)
+		return;
+	
+	//fprintf(stderr, "Observer_new: %p %d %s (%s %p)\n", THIS, OBJECT_class(object)->n_event, name, GB_GetClassName(parent), parent);
+	
+	ev = OBJECT_event(object);
+  
+  THIS->after = VARGOPT(after, FALSE);
+  
+	ALLOC_ZERO(&THIS->event, sizeof(ushort) * class->n_event, "Observer_new");
+
+	THIS->object = object;
+	OBJECT_attach((OBJECT *)THIS, parent, name);
+	COBSERVER_attach(THIS, parent, name);
+
+	LIST_insert((void **)&ev->observer, THIS, &THIS->list);
+  OBJECT_REF(THIS, "Observer_new");
+	
+	#if 0
+	if (proxy)
+	{
+		// The proxy object is not referenced. It means that destroying the proxy implies
+		// that the observer won't raise events anymore. IS IT SURE?
+		THIS->proxy = proxy;
+	}
+	#endif
+
+END_METHOD
+
+
+BEGIN_METHOD_VOID(Observer_free)
+
+	//fprintf(stderr, "Observer_free: %p\n", this);
+	
+	GB_StoreVariant(NULL, &THIS->tag);
+	COBSERVER_detach(THIS);
+
+END_METHOD
+
+BEGIN_PROPERTY(Observer_Object)
+
+	GB_ReturnObject(THIS->object);
+	
+END_PROPERTY
+
+BEGIN_PROPERTY(Observer_Tag)
+
+	if (READ_PROPERTY)
+		GB_ReturnPtr(GB_T_VARIANT, &THIS->tag);
+	else
+		GB_StoreVariant(PROP(GB_VARIANT), &THIS->tag);
+	
+END_PROPERTY
+
+#endif
+
+GB_DESC NATIVE_Observer[] =
+{
+  GB_DECLARE("Observer", sizeof(COBSERVER)), 
+
+  GB_METHOD("_new", NULL, Observer_new, "(Object)o[(After)b]"),
+  GB_METHOD("_free", NULL, Observer_free, NULL),
+  
+  GB_PROPERTY_READ("Object", "o", Observer_Object),
+  GB_PROPERTY("Tag", "v", Observer_Tag),
+  
+  GB_END_DECLARE
+};
+
+#if 0
+GB_DESC NATIVE_Proxy[] =
+{
+  GB_DECLARE("Proxy", sizeof(COBSERVER)), 
+
+  GB_METHOD("_new", NULL, Observer_new, "(Object)o[(After)b]"),
+  GB_METHOD("_free", NULL, Observer_free, NULL),
+  
+  GB_END_DECLARE
+};
+#endif
---
+++ main/gbx/gbx_c_observer.h
@@ -0,0 +1,57 @@
+/***************************************************************************
+
+  gbx_c_observer.h
+
+  (c) 2000-2009 Benoît Minisini <gambas@users.sourceforge.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+***************************************************************************/
+
+#ifndef __GBX_C_OBSERVER_H
+#define __GBX_C_OBSERVER_H
+
+#include "gambas.h"
+#include "gbx_object.h"
+#include "gb_list.h"
+
+#ifndef __GBX_C_OBSERVER_C
+extern GB_DESC NATIVE_Observer[];
+//extern GB_DESC NATIVE_Proxy[];
+#else
+#define THIS ((COBSERVER *)_object)
+#endif
+
+// Note: the interpreter automatically allocates an extra OBJECT_EVENT structure. See CLASS_calc_info().
+
+typedef
+	struct {
+		OBJECT ob;
+		LIST list;
+		ushort *event;
+		void *object;
+		void *tag;
+		unsigned after : 1;
+		unsigned locked : 1;
+	}
+	COBSERVER;
+
+void COBSERVER_attach(COBSERVER *this, void *parent, const char *name);
+void COBSERVER_detach(COBSERVER *this);
+
+#define COBSERVER_lock(_this, _lock) (((COBSERVER *)_this)->locked = (_lock))
+#define COBSERVER_is_locked(_this) (((COBSERVER *)_this)->locked)
+
+#endif
