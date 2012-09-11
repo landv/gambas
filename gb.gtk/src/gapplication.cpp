e33d728ac3a81af0142e5687167e321f3bf44e49
Benoît Minisini <gambas@users.sourceforge.net>
2012-09-11 20:59:20 +0000

    [GB.GTK] * BUG: Fix event management of modal dialogs.
    git-svn-id: svn://localhost/gambas/trunk@5158 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- gb.gtk/src/gapplication.cpp
+++ gb.gtk/src/gapplication.cpp
@@ -209,15 +209,8 @@ static void gambas_handle_event(GdkEvent *event)
 		grab = gApplication::_popup_grab;
 		//gdk_window_get_user_data(gApplication::_popup_grab_window, (gpointer *)&grab);
 	
-	/*if (event->type == GDK_BUTTON_PRESS || event->type == GDK_BUTTON_RELEASE)
-	{
-		fprintf(stderr, "widget = %p grab = %p _popup_grab = %p _button_grab = %p\n", widget, grab, gApplication::_popup_grab, gApplication::_button_grab);
-		//fprintf(stderr, "widget = %p (%p) grab = %p (%p)\n", widget, widget ? g_object_get_data(G_OBJECT(widget), "gambas-control") : 0, 
-		//				grab, grab ? g_object_get_data(G_OBJECT(grab), "gambas-control") : 0);
-	}*/
-	
-	if (grab && !gApplication::_popup_grab && !gApplication::_button_grab)
-		goto __HANDLE_EVENT;
+	//if (grab && !gApplication::_popup_grab && !gApplication::_button_grab)
+	//	goto __HANDLE_EVENT;
 		
 	if (event->type == GDK_FOCUS_CHANGE)
 	{
@@ -247,11 +240,15 @@ static void gambas_handle_event(GdkEvent *event)
 		goto __HANDLE_EVENT;
 	}
 	
-	if (grab)
+	/*if (event->type == GDK_BUTTON_PRESS || event->type == GDK_BUTTON_RELEASE)
 	{
-		if (widget != grab && !gtk_widget_is_ancestor(widget, grab))
-			widget = grab;
-	}
+		fprintf(stderr, "widget = %p grab = %p _popup_grab = %p _button_grab = %p\n", widget, grab, gApplication::_popup_grab, gApplication::_button_grab);
+		//fprintf(stderr, "widget = %p (%p) grab = %p (%p)\n", widget, widget ? g_object_get_data(G_OBJECT(widget), "gambas-control") : 0, 
+		//				grab, grab ? g_object_get_data(G_OBJECT(grab), "gambas-control") : 0);
+	}*/
+	
+	if (grab && widget != grab && !gtk_widget_is_ancestor(widget, grab))
+		widget = grab;
 	
 	//real = true;
 	while (widget)
@@ -266,14 +263,14 @@ static void gambas_handle_event(GdkEvent *event)
 	/*if (event->type == GDK_BUTTON_PRESS)
 		fprintf(stderr, "GDK_BUTTON_PRESS: %p %s\n", widget, control ? control->name() : NULL);
 	else if (event->type == GDK_BUTTON_RELEASE)
-		fprintf(stderr, "GDK_BUTTON_RELEASE: %p %s\n", widget, control ? control->name() : NULL);
-	*/
+		fprintf(stderr, "GDK_BUTTON_RELEASE: %p %s\n", widget, control ? control->name() : NULL);*/
+	
 	/*else if (event->type == GDK_KEY_PRESS)
 		fprintf(stderr, "GDK_KEY_PRESS: %p %s (%s)\n", widget, control ? control->name() : NULL, gApplication::activeControl() ? gApplication::activeControl()->name() : NULL);
 	*/
 	
-	if (grab && widget != grab)
-		goto __HANDLE_EVENT;
+	/*if (grab && widget != grab)
+		goto __HANDLE_EVENT;*/
 	
 	if (!widget || !control)
 		goto __HANDLE_EVENT;
