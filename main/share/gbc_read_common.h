34de79ef3d568c632954229fc8dda41edbee72d4
Benoît Minisini <gambas@users.sourceforge.net>
2012-11-06 20:11:20 +0000

    [DEVELOMENT ENVIRONMENT] * BUG: Disable embedded http server.
    [INTERPRETER]
    * BUG: Fix a crash in String.Comp() when one of the two string is null.
    
    [GB.FORM]
    * BUG: Don't show useless menus in the DirView popup menu of a DirChooser 
      or FileChooser control.
    * BUG: Do not scroll the TabPanel tab bar if not really necessary.
    
    [GB.GUI.BASE]
    * BUG: For backward-compatibility reasons, you can now set the value of 
      any arbitrary positive column index of a ColumnView item.
    
    
    git-svn-id: svn://localhost/gambas/trunk@5301 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- main/share/gbc_read_common.h
+++ main/share/gbc_read_common.h
@@ -1,23 +1,23 @@
 /***************************************************************************
 
-  gbc_read_common.h
+	gbc_read_common.h
 
-  (c) 2000-2012 Benoît Minisini <gambas@users.sourceforge.net>
+	(c) 2000-2012 Benoît Minisini <gambas@users.sourceforge.net>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or (at your option)
-  any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-  MA 02110-1301, USA.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+	MA 02110-1301, USA.
 
 ***************************************************************************/
 
@@ -25,27 +25,27 @@
 #define __GBC_READ_COMMON_H
 
 typedef
-  uint PATTERN;
+	uint PATTERN;
 
 enum {
-  RT_END = 0,
-  RT_NEWLINE = 1,
-  RT_RESERVED = 2,
-  RT_IDENTIFIER = 3,
-  RT_NUMBER = 4,
-  RT_STRING = 5,
-  RT_TSTRING = 6,
-  RT_PARAM = 7,
-  RT_SUBR = 8,
-  RT_CLASS = 9,
-  RT_COMMENT = 10, /* Used by Eval() */
-  RT_OPERATOR = 11, /* Used by Eval() */
-  RT_COMMAND = 12,
-  RT_OUTPUT = 0x20,
-  RT_POINT = 0x40,
-  RT_FIRST = 0x80
-  };
-   
+	RT_END = 0,
+	RT_NEWLINE = 1,
+	RT_RESERVED = 2,
+	RT_IDENTIFIER = 3,
+	RT_NUMBER = 4,
+	RT_STRING = 5,
+	RT_TSTRING = 6,
+	RT_PARAM = 7,
+	RT_SUBR = 8,
+	RT_CLASS = 9,
+	RT_COMMENT = 10, /* Used by Eval() */
+	RT_OPERATOR = 11, /* Used by Eval() */
+	RT_COMMAND = 12,
+	RT_OUTPUT = 0x20,
+	RT_POINT = 0x40,
+	RT_FIRST = 0x80
+	};
+	
 enum {
 	RC_SECTION = 1
 	};
@@ -66,7 +66,7 @@ enum {
 #define PATTERN_is_reserved(pattern)    (PATTERN_type(pattern) == RT_RESERVED)
 
 #define PATTERN_is_identifier(pattern)  (PATTERN_type(pattern) == RT_IDENTIFIER)
-#define PATTERN_is_class(pattern) 		  (PATTERN_type(pattern) == RT_CLASS)
+#define PATTERN_is_class(pattern)       (PATTERN_type(pattern) == RT_CLASS)
 #define PATTERN_is_newline(pattern)     (PATTERN_type(pattern) == RT_NEWLINE)
 #define PATTERN_is_param(pattern)       (PATTERN_type(pattern) == RT_PARAM)
 #define PATTERN_is_subr(pattern)        (PATTERN_type(pattern) == RT_SUBR)
@@ -74,6 +74,7 @@ enum {
 #define PATTERN_is_string(pattern)      (PATTERN_type(pattern) == RT_STRING)
 #define PATTERN_is_tstring(pattern)     (PATTERN_type(pattern) == RT_TSTRING)
 #define PATTERN_is_command(pattern)     (PATTERN_type(pattern) == RT_COMMAND)
+#define PATTERN_is_comment(pattern)     (PATTERN_type(pattern) == RT_COMMENT)
 
 #define PATTERN_is_newline_end(pattern) (PATTERN_is_newline(pattern) || PATTERN_is_end(pattern))
 
