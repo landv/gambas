1a804d1e45f6a94d344e0fc8dd5dd2815247767f
Benoît Minisini <gambas@users.sourceforge.net>
2015-07-07 20:51:57 +0000

    [DEVELOPMENT ENVIRONMENT] * NEW: The "Show search window" button in the search list does not reset   the searching context to the current procedure anymore. It keeps the   current context unchanged instead.
    [GB.DRAW]
    * NEW: Paint.ZoomImage used dash pattern to draw the zoom grid.
    
    [GB.FORM]
    * NEW: ImageView uses #808080 as grid color.
    
    [GB.FORM.EDITOR]
    * BUG: TextEditor: Don't reset an editor getting the focus. Do it only if 
      needed.
    
    
    git-svn-id: svn://localhost/gambas/trunk@7175 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- main/lib/draw/cpaint.c
+++ main/lib/draw/cpaint.c
@@ -1491,11 +1491,29 @@ BEGIN_METHOD(Paint_ZoomImage, GB_OBJECT image; GB_INTEGER zoom; GB_INTEGER x; GB
 	
 	if (border && zoom >= 3)
 	{
+		float dashes[2] = { 1.0, 1.0 };
+		int count = 2;
+		float *pdashes = dashes;
+	
+		PAINT->Dash(THIS, TRUE, &pdashes, &count);
+		PAINT->Background(THIS, TRUE, &borderColor);
+
 		for (i = sx, xr = x; i < (sx + sw); i++, xr += zoom)
-			PAINT->FillRect(THIS, xr, y, 1, sh * zoom, borderColor);
+		{
+			//PAINT->FillRect(THIS, xr, y, 1, sh * zoom, borderColor);
+			PAINT->MoveTo(THIS, xr, y);
+			PAINT->LineTo(THIS, xr, y + sh * zoom);
+		}
 		
 		for (j = sy, yr = y; j < (sy + sh); j++, yr += zoom)
-			PAINT->FillRect(THIS, x, yr, sw * zoom, 1, borderColor);
+		{
+			//PAINT->FillRect(THIS, x, yr, sw * zoom, 1, borderColor);
+			PAINT->MoveTo(THIS, x, yr);
+			PAINT->LineTo(THIS, x + sw * zoom, yr);
+		}
+		
+		PAINT->Stroke(THIS, FALSE);
+		THIS->has_path = FALSE;
 	}
 	
 	PAINT->Restore(THIS);
