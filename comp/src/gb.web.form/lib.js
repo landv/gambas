3ec3e2b9b5ee0341053dcc64ff9e0f15ec876e3c
Benoît Minisini <gambas@users.sourceforge.net>
2015-11-11 00:45:49 +0000

    [GB.WEB.FORM] * NEW: Button.Enabled is a new property that allows to disable a button. * BUG: Fix container style generation. * NEW: WebControl have a Width and Height properties now, which are   synoymous for their style property counterparts. * OPT: WebControl do not send events if there are no event handler inside   the server code. * NEW: WebTextBox is a new control that implements a text box. * NEW: New 'update' message from the client to update a control property in   the server. * NEW: WebForm.Close() is a new method that resets the form state, and try   to close it. Note that the browser prevent windows to be closed by a   script if they haven't been opened by a script. * NEW: Control refresh in the browser tries to preserve the current active   element and its selection.
    git-svn-id: svn://localhost/gambas/trunk@7464 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- comp/src/gb.web.form/lib.js
+++ comp/src/gb.web.form/lib.js
@@ -12,8 +12,20 @@ gw = {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200 && xhr.responseText)
       {
+        gw.active = document.activeElement.id;
+        if (gw.active)
+          gw.selection = gw.getSelection($(gw.active));
+        else
+          gw.selection = undefined;
+        
         console.log(xhr.responseText);
         eval(xhr.responseText);
+        
+        if (gw.active)
+        {
+          $(gw.active).focus();
+          gw.setSelection($(gw.active), gw.selection);
+        }
       }
     };
     xhr.send(null);
@@ -23,4 +35,41 @@ gw = {
   {
     gw.send(form, ['raise', id, event, args]);
   },
-};
+  
+  update: function(form, id, prop, value)
+  {
+    gw.send(form, ['update', id, prop, value]);
+  },
+
+  getSelection: function(o)
+  {
+    var start, end;
+    
+    if (o.createTextRange) 
+    {
+      var r = document.selection.createRange().duplicate();
+      r.moveEnd('character', o.value.length)
+      if (r.text == '')
+        start = o.value.length;
+      else
+        start = o.value.lastIndexOf(r.text);
+      r.moveStart('character', -o.value.length);
+      end = r.text.length;
+      return [start, end];
+    }
+    
+    if (o.selectionStart && o.selectionEnd)
+      return [o.selectionStart, o.selectionEnd];
+    
+    return null;
+  },
+  
+  setSelection: function(o, sel)
+  {
+    if (sel)
+    {
+      if (o.setSelectionRange)
+        o.setSelectionRange(sel[0], sel[1]);
+    }
+  }
+}
