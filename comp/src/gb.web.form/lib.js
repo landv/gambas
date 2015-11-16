2ccdb3336ccd8ec8cd57317427480959e67f3979
Benoît Minisini <gambas@users.sourceforge.net>
2015-11-16 06:07:06 +0000

    [GB.WEB.FORM] * NEW: WebControl.Raise() is a new method to raise a control in its parent   hierarchy. * NEW: WebControl.Lower() is a new method to lower a control in its parent   hierarchy. * NEW: WebControl.Delete() deletes a control. * NEW: Support for reisable windows. * NEW: WebForm.Open() is a new method to open a child form as a window   inside the current form.
    git-svn-id: svn://localhost/gambas/trunk@7479 867c0c6c-44f3-4631-809d-bfa615b0a4ec

--- comp/src/gb.web.form/lib.js
+++ comp/src/gb.web.form/lib.js
@@ -6,12 +6,14 @@ function $(a)
 gw = {
 
   timers: {},
+  windows: [],
+  form: '',
 
-  send: function(form, command)
+  send: function(command)
   {
     var xhr = new XMLHttpRequest();
     console.log(command);
-    xhr.open('GET', $root + '/' + form + '/x?c=' + encodeURIComponent(JSON.stringify(command)), true);
+    xhr.open('GET', $root + '/' + encodeURIComponent(gw.form) + '/x?c=' + encodeURIComponent(JSON.stringify(command)), true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200 && xhr.responseText)
       {
@@ -26,22 +28,27 @@ gw = {
         
         if (gw.active)
         {
-          $(gw.active).focus();
-          gw.setSelection($(gw.active), gw.selection);
+          if ($(gw.active))
+          {
+            $(gw.active).focus();
+            gw.setSelection($(gw.active), gw.selection);
+          }
+          else
+            gw.active = document.activeElement.id;
         }
       }
     };
     xhr.send(null);
   },
 
-  raise: function(form, id, event, args)
+  raise: function(id, event, args)
   {
-    gw.send(form, ['raise', id, event, args]);
+    gw.send(['raise', id, event, args]);
   },
   
-  update: function(form, id, prop, value)
+  update: function(id, prop, value)
   {
-    gw.send(form, ['update', id, prop, value]);
+    gw.send(['update', id, prop, value]);
   },
 
   getSelection: function(o)
@@ -89,23 +96,268 @@ gw = {
   
   resizeComboBox: function(id)
   {
-    $(id + '-select').style.width = $(id).offsetWidth + 'px';
+    $(id + '-select').onmouseover = function() { $(id + '-select').style.width = $(id).offsetWidth + 'px'; }
   },
   
-  addTimer: function(form, id, delay)
+  addTimer: function(id, delay)
   {
-    gw.removeTimer(form, id);
-    gw.timers[form + '.' + id] = setInterval(function() { gw.raise(form, id, 'timer'); }, delay);
+    gw.removeTimer(id);
+    gw.timers[id] = setInterval(function() { gw.raise(id, 'timer'); }, delay);
   },
   
-  removeTimer: function(form, id)
+  removeTimer: function(id)
   {
-    var t = gw.timers[form + '.' + id];
+    var t = gw.timers[id];
     if (t)
     {
-      clearInterval(gw.timers[form + '.' + id]);
-      gw.timers[form + '.' + id] = undefined;
+      clearInterval(gw.timers[id]);
+      gw.timers[id] = undefined;
+    }
+  },
+  
+  window: 
+  {
+    open: function(id)
+    {
+      gw.window.close(id);
+
+      if (gw.windows.length == 0)
+      {
+        document.addEventListener('mousemove', gw.window.onMove);
+        document.addEventListener('mouseup', gw.window.onUp);
+        console.log('document.addEventListener');
+      }
+      
+      $(id).style.zIndex = 10 + gw.windows.length;
+      gw.windows.push(id);
+      
+      $(id).addEventListener('mousedown', gw.window.onMouseDown);
+      // Touch events 
+      //pane.addEventListener('touchstart', onTouchDown);
+      //document.addEventListener('touchmove', onTouchMove);
+      //document.addEventListener('touchend', onTouchEnd);
+    },
+    
+    close: function(id)
+    {
+      var i;
+      
+      $(id).removeEventListener('mousedown', gw.window.onMouseDown);
+      
+      i = gw.windows.indexOf(id);
+      if (i >= 0)
+      {
+        gw.windows.splice(i, 1);
+        gw.window.refresh();
+      }
+      
+    },
+    
+    refresh: function()
+    {
+      var i = 0;
+      
+      while (i < gw.windows.length)
+      {
+        if ($(gw.windows[i]))
+        {
+          $(gw.windows[i]).style.zIndex = 10 + i;
+          i++;
+        }
+        else
+          gw.windows.splice(i, 1);
+      }
+      
+      if (gw.windows.length == 0)
+      {
+        console.log('document.removeEventListener');
+        document.removeEventListener('mousemove', gw.window.onMove);
+        document.removeEventListener('mouseup', gw.window.onUp);
+      }
+    },
+    
+    raise: function(id)
+    {
+      var i = gw.windows.indexOf(id);
+      if (i < 0)
+        return;
+      
+      gw.windows.splice(i, 1);
+      gw.windows.push(id);
+      
+      for (i = 0; i < gw.windows.length; i++)
+        $(gw.windows[i]).style.zIndex = 10 + i;
+        
+      gw.update('', '#windows', gw.windows);
+    },
+    
+    onMouseDown: function(e)
+    {
+      gw.window.onDown(e);
+    },
+    
+    onDown: function(e)
+    {
+      var c;
+      
+      if (e.target.className == 'gw-window-button')
+        return;
+      
+      gw.window.onMove(e);
+      
+      c = gw.window.context;
+      if (c && (c.isMoving || c.isResizing))
+      {
+        gw.window.raise(c.id);
+        gw.window.downEvent = e;
+        e.preventDefault();
+      }
+    },
+    
+    onMove(e) 
+    {
+      var i, id, elt, b, x, y, bx, by, bw, bh;
+      var onTopEdge, onLeftEdge, onRightEdge, onBottomEdge, isResizing;
+      var MARGINS = 6;
+      
+      if (gw.window.downEvent)
+      {
+        gw.window.context.cx = e.clientX;
+        gw.window.context.cy = e.clientY;
+        gw.window.animate();
+        return;
+      }
+      
+      gw.window.context = undefined;
+      
+      for (i = 0; i < gw.windows.length; i++)
+      {
+        id = gw.windows[gw.windows.length - i - 1]
+        elt = $(id);
+        b = elt.getBoundingClientRect();
+        
+        bx = b.left; // - MARGINS;
+        by = b.top; // - MARGINS;
+        bw = b.width; // + MARGINS * 2;
+        bh = b.height; // + MARGINS * 2;
+        
+        x = e.clientX - bx;
+        y = e.clientY - by;
+        
+        console.log(x + ',' + y + ' : ' + bx + ',' + by + ',' + bw + ',' + bh);
+        
+        if (x >= 0 && x < bw && y >= 0 && y < bh)
+        {
+          onTopEdge = y < MARGINS;
+          onLeftEdge = x < MARGINS;
+          onRightEdge = x >= (bw - MARGINS);
+          onBottomEdge = y >= (bh - MARGINS);
+          
+          isResizing = onTopEdge || onLeftEdge || onRightEdge || onBottomEdge;
+          isMoving = !isResizing && y < ($(id + '-titlebar').offsetHeight + MARGINS);
+          
+          gw.window.context = {
+            id: id,
+            x: b.left + window.scrollX,
+            y: b.top + window.scrollY,
+            cx: e.clientX,
+            cy: e.clientY,
+            w: b.width,
+            h: b.height,
+            isResizing: isResizing,
+            isMoving: isMoving,
+            onTopEdge: onTopEdge,
+            onLeftEdge: onLeftEdge,
+            onRightEdge: onRightEdge,
+            onBottomEdge: onBottomEdge
+            };
+          gw.window.animate();
+          break;
+        }
+      }
+    },
+    
+    onUp: function(e)
+    {
+      var c = gw.window.context;
+      
+      gw.window.downEvent = undefined;
+      
+      if (c && (c.isMoving || c.isResizing))
+      {
+        var id = gw.window.context.id;
+        var b = $(id).getBoundingClientRect();
+        gw.window.context = undefined;
+        gw.update(id, '#geometry', [ b.left + 'px', b.top + 'px', b.width + 'px', b.height + 'px']);
+      }
+    },
+
+    animate: function() 
+    {
+      var id, elt, c, e, x, y, w, h;
+      var minWidth = 120;
+      var minHeight;
+      
+      c = gw.window.context;
+      if (!c) return;
+    
+      elt = $(c.id);
+      minHeight = $(c.id + '-titlebar').offsetHeight + 2;
+      e = gw.window.downEvent;
+    
+      if (c && c.isResizing && e)
+      {
+        if (c.onRightEdge)
+          elt.style.width = Math.max(c.w + c.cx - e.clientX, minWidth) + 'px';
+        
+        if (c.onBottomEdge)
+          elt.style.height = Math.max(c.h + c.cy - e.clientY, minHeight) + 'px';
+    
+        if (c.onLeftEdge) 
+        {
+          x = c.x + c.cx - e.clientX;
+          w = c.x + c.w - x;
+          if (w >= minWidth) 
+          {
+            elt.style.width = w + 'px';
+            elt.style.left = x + 'px'; 
+          }
+        }
+    
+        if (c.onTopEdge) 
+        {
+          y = c.y + c.cy - e.clientY;
+          h = c.y + c.h - y;
+          if (h >= minHeight) 
+          {
+            elt.style.height = h + 'px';
+            elt.style.top = y + 'px'; 
+          }
+        }
+    
+        return;
+      }
+    
+      if (c && c.isMoving && e)
+      {
+        elt.style.left = (Math.max(0, c.x + c.cx - e.clientX)) + 'px';
+        elt.style.top = (Math.max(0, c.y + c.cy - e.clientY)) + 'px';
+        return;
+      }
+    
+      // This code executes when mouse moves without clicking
+    
+      // style cursor
+      if (c.onRightEdge && c.onBottomEdge || c.onLeftEdge && c.onTopEdge)
+        elt.style.cursor = 'nwse-resize';
+      else if (c.onRightEdge && c.onTopEdge || c.onBottomEdge && c.onLeftEdge)
+        elt.style.cursor = 'nesw-resize';
+      else if (c.onRightEdge || c.onLeftEdge)
+        elt.style.cursor = 'ew-resize';
+      else if (c.onBottomEdge || c.onTopEdge)
+        elt.style.cursor = 'ns-resize';
+      else
+        elt.style.cursor = '';
     }
   }
-  
 }
