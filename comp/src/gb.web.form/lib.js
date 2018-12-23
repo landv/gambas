76a15e14ffaa6f8d487161a55ce27f6bcfce97c5
gambas <g4mba5@gmail.com>
2018-12-23 19:37:56 +0100

    WebTextBox: Correctly synchronize text before raising Activate event.
    [GB.WEB.FORM]
    * BUG: WebTextBox: Correctly synchronize text before raising Activate event.

--- comp/src/gb.web.form/lib.js
+++ comp/src/gb.web.form/lib.js
@@ -1386,13 +1386,13 @@ gw = {
     onactivate: function(id, e)
     {
       if (e.keyCode == 13)
-        setTimeout(function() { gw.raise(id, 'activate', [], false); }, 50);
+        setTimeout(function() { gw.update(id, 'text', $(id + ':entry').value); gw.raise(id, 'activate', [], false); }, 50);
     },
     
     onchange: function(id)
     {
       if ($(id).gw_timer) clearTimeout($(id).gw_timer);
-      $(id).gw_timer = setTimeout(function() { gw.update(id, 'text', $(id + ':entry').value, null); }, 50);
+      $(id).gw_timer = setTimeout(function() { gw.update(id, 'change', $(id + ':entry').value, null); }, 50);
     },
     
     getText: function(id)
@@ -1427,7 +1427,7 @@ gw = {
     onchange: function(id)
     {
       if ($(id).gw_timer) clearTimeout($(id).gw_timer);
-      $(id).gw_timer = setTimeout(function() { gw.update(id, 'text', $(id).value, null); }, 50);
+      $(id).gw_timer = setTimeout(function() { gw.update(id, 'change', $(id).value, null); }, 50);
     },
     
     copy: function(id)
