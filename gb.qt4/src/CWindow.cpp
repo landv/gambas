3d9f5cc9141846fc7a8690e358b278372b5afde3
Laurent Carlier <lordheavym@gmail.com>
2019-11-02 16:34:06 +0100

    Fix compiler warning in gb.qt5 about deprecation of QDesktopWidget::primaryScreen()
    QApplication::desktop()->primaryScreen() is deprecated since Qt 5.11
    Use QGuiApplication::primaryScreen() instead.

--- gb.qt4/src/CWindow.cpp
+++ gb.qt4/src/CWindow.cpp
@@ -2483,6 +2483,11 @@ void MyMainWindow::doReparent(QWidget *parent, const QPoint &pos)
 
 int MyMainWindow::currentScreen() const
 {
+#ifdef QT5
+	QList<QScreen*> screenList;
+	QScreen* primaryScreen;
+#endif
+
 	if (_screen >= 0)
 		return _screen;
 
@@ -2491,7 +2496,15 @@ int MyMainWindow::currentScreen() const
 	else if (CWINDOW_Main)
 		return QApplication::desktop()->screenNumber(CWINDOW_Main->widget.widget);
 	else
+#ifndef QT5
 		return QApplication::desktop()->primaryScreen();
+#else
+	{
+		primaryScreen = QGuiApplication::primaryScreen();
+		screenList = QGuiApplication::screens();
+		return screenList.indexOf(primaryScreen);
+	}
+#endif
 }
 
 void MyMainWindow::center()
